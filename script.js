ddf9b914b15e2d11483ba9369c0eed8c7ba12f67
Buse Köseoğlu <44964158+busekoseoglu@users.noreply.github.com>
2023-12-09 22:13:59 +0300

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+function openAboutPage() {
+    // Change 'aboutmePage.html' to the actual filename or path of your about page
+    window.location.href = 'aboutmePage.html';
+}
+
+function openMainPage() {
+    // Change 'aboutmePage.html' to the actual filename or path of your about page
+    window.location.href = 'index.html';
+}
+
+function openBlogPage() {
+    // Change 'aboutmePage.html' to the actual filename or path of your about page
+    window.location.href = 'blogsPage.html';
+}
+
+function openContactPage() {
+    // Change 'aboutmePage.html' to the actual filename or path of your about page
+    window.location.href = 'contactPage.html';
+}
